e59630fc584bd75eb0a4600a205cb4149a7ac06c
Maxwell Horton <maxthomashorton@gmail.com>
2021-09-30 19:51:00 -0400

    fixed everything

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
 const {Sequelize} = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl:{
+                require: true,
+                rejectUnauthorized: false
+            }
+}
+
 });
 
 module.exports = db;
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const User = db.define("user", {
+    username: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+});
+
+module.exports = User;
+
